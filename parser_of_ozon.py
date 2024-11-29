8ffd3d8338ae1bdc385cde6dffa2b7d02b3dabaa
Kirill <zeverus314@mail.ru>
2024-11-30 00:43:42 +0500

    add function get_info_about_card to send a request to the url of each product and handle possible errors

--- parser_of_ozon.py
+++ parser_of_ozon.py
@@ -1,11 +1,14 @@
 import aiohttp
-import requests
+# import requests
 from bs4 import BeautifulSoup
 import asyncio
 from selenium import webdriver
 from selenium_stealth import stealth
 import undetected_chromedriver as uc
 import time
+from curl_cffi import requests
+import json
+
 
 
 url = 'https://www.ozon.ru/category/smartfony-15502/'
@@ -51,14 +54,43 @@ def get_page_cards(driver, url):
 
             card_url_value = card_url["href"]
 
-            if card_name is None:
-                print("Card name not found in card")
-                continue
+            # if card_name is None:
+            #     print("Card name not found in card")
+            #     continue
 
-            card_name_value = card_name.text
-            print(f'Ссылка на карточку: {'https://www.ozon.ru'+card_url_value}, Название карточки: {card_name_value}')
+            # card_name_value = card_name.text
+            get_info_about_card(card_url_value)
+        
+
+def get_info_about_card(card_url_value):
+    session = requests.Session()
+    try:
+        raw_data = session.get(f'https://www.ozon.ru/api/composer-api.bx/page/json/v2?url={card_url_value}')
+        raw_data.raise_for_status()  # Проверка на успешный HTTP-ответ
+        json_data = raw_data.json()  # Метод .json() автоматически декодирует JSON
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+        return
+    
+    except json.JSONDecodeError as e:
+        print(f"Ошибка декодирования JSON: {e}")
+        return
+
+    if json_data is None:
+        print("Card is not found")
+        return
+
+    # full_name = json_data['seo']['title']
+    # print(full_name)
+    product = json_data['seo']['title']
 
 
+def collect_information_about_products(product_url, article, marketplace, 
+                                       product_name, price_without_discount, 
+                                       price_with_discount, quantity_of_goods) -> list:
+    
+    
 
         
 
